0ab9b23a94a1626cfd92dae5e014219256eacdf6
Lawrence Lagerlof <llagerlof@gmail.com>
2017-06-20 09:53:01 -0300

    Added INSERT capability.

--- MyCrudPHP.php
+++ MyCrudPHP.php
@@ -8,22 +8,23 @@ class MyCrudPHP {
 	private $record;
 	private $values;
 	private $execution_log;
+	private $table_structure;
 
 	function __construct($conn) {
-		$this->log = new MyLogPHP();
+		$this->execution_state = 'INSERT';
 		$paramType = gettype($conn);
 		if ($paramType == 'object') {
 			$this->conn = $conn;
 		} else if ($paramType == 'array') {
 			$this->conn = new PDO($conn['statement'], $conn['user'], $conn['password']);
-		} 
+		}
 	}
 
 	// Return a table structure.
 	private function getTableStructure($table) {
 		$sql = "desc " . $table;
 		$ps = $this->conn->prepare($sql);
-		$this->addExecutionLog(array($trim(sql)));
+		$this->addExecutionLog(array(trim($sql)));
 		$ps->execute();
 		$rs = $ps->fetchAll();
 		return $rs;
@@ -65,6 +66,7 @@ class MyCrudPHP {
 
 	public function save() {
 		if ( ($this->execution_state == 'UPDATE') && (count($this->values) > 0) && (count($this->filter) > 0) ) {
+			
 			// Filter
 			$where = "";
 			$counter = 1;
@@ -107,11 +109,33 @@ class MyCrudPHP {
 			} else {
 				return false;
 			}
+		} elseif ( ($this->execution_state == 'INSERT') && (count($this->values) > 0) ) {
+			$params_comma = $this->getFieldsParametersComma();
+			$fields_params = array_keys($this->values);
+			$fields_record = array_keys($this->record[0]);
+			$intersection = array_intersect($fields_params, $fields_record);
+			$fields_commas = implode(', ', $intersection);
+
+			$sql = "
+				insert into ". $this->table_name . " (" . $fields_commas . ") values (". $params_comma .")
+			";
+			$q = $this->conn->prepare($sql);
+			$bound = $this->values;
+			$this->addExecutionLog(array(trim($sql), $bound));
+
+			if ($q->execute($bound)) {
+				$this->values = array();
+				return true;				
+			} else {
+				return false;
+			}
+			
 		} else {
+			
 			if (($this->execution_state != 'UPDATE') || ($this->execution_state != 'INSERT')) {
 				throw new Exception('Not in UPDATE or INSERT state.');
 			} else {
-				return true;
+				return false;
 			}
 		}
 	}
@@ -124,22 +148,53 @@ class MyCrudPHP {
 		return $this->execution_log;
 	}
 	
-	public function out() {
-		print_r($this->record);
-	}
-	
 	public function getLoadedRecord() {
 		return $this->record;
+	}
+
+	private function getFieldsParametersComma() {
+		$params = "";
+		$counter = 1;
+		$number_of_fields = count($this->values);
+		foreach ($this->values as $field => $value) {
+			$params .= ":" . $field;
+			if ($counter < $number_of_fields) {
+				$params .= ", ";
+			}
+			$counter++;
+		}
+		return $params;
+	}
+	
+	public function newRecord() {
+		$this->table_structure = $this->getTableStructure($this->table_name);
+		foreach ($this->table_structure as $field) {
+			$this->record[0][$field['Field']] = null;
+		}
+		$this->execution_state = 'INSERT';
+		return $this;
 	}
 }
 
 /*
-// Example:
+// Examples
 $PDO = new PDO( "mysql:host=127.0.0.1;port=3306;dbname=database", 'root', null);
 $crud = new MyCrudPHP($PDO);
-$person = $crud->table('persons')->getRecord(array('id_person' => 1));
-$pessoa->setValues(array('name' => 'Lawrence'));
-$pessoa->save();
+
+// Query:
+$person = $crud->table('persons')->getRecord(array('id' => 1));
+print_r($person->getLoadedRecord());
+
+// Update:
+$person = $crud->table('persons')->getRecord(array('id' => 1));
+$person->setValues(array('name' => 'Lawrence', 'age' => 27));
+$person->save();
+
+// Insert:
+$person = $crud->table('persons')->newRecord();
+$person->setValues(array('name' => 'Lawrence', 'age' => 27));
+$person->save();
+
 */
 
 ?>
